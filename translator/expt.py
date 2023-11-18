c6c57155a191447ae5d2367ed9d015af441a594c
arisia <starlights2283@gmail.com>
2023-11-19 00:25:30 +0900

    update python file

--- translator/expt.py
+++ translator/expt.py
@@ -9,7 +9,6 @@ text_encoder = CLIPTextModel.from_pretrained(model_id, subfolder="text_encoder",
 vae = AutoencoderKL.from_pretrained(model_id, subfolder="vae", torchscript=True, return_dict=False)
 unet_model = UNet2DConditionModel.from_pretrained(model_id, subfolder="unet", torchscript=True, return_dict=False)
 tokenizer = AutoTokenizer.from_pretrained(model_id, subfolder="tokenizer")
-print(scheduler, text_encoder, unet_model, tokenizer)
 
 vae.eval()
 unet_model.eval()
@@ -20,37 +19,30 @@ tokenized = tokenizer(prompt,
                       padding="max_length", max_length=tokenizer.model_max_length,
                       truncation=True, return_tensors="pt")
 
-print(tokenized)
-
 text_embeddings = text_encoder(**tokenized)
-traced_text = torch.jit.trace(text_encoder, (tokenized["input_ids"], tokenized['attention_mask']))
 
 latents = torch.randn((1, 4, 64, 64))
 latent_model_input = torch.cat([latents] * 2)
 text_embeddings = torch.stack([text_embeddings[0], text_embeddings[0]]).squeeze()
 
-
 def forward(self, sample, timestep, encoder_hidden_states,
             class_labels=None, return_dict: bool = False, ):
     return UNet2DConditionModel.forward(self, sample, timestep, encoder_hidden_states, class_labels, return_dict)
 
-
 unet_model.forward = forward.__get__(unet_model, UNet2DConditionModel)
-traced_unet = torch.jit.trace(unet_model, (latent_model_input, torch.tensor([981]), text_embeddings))
+scripted_unet = torch.jit.script(unet_model)
 
 def encode(self, x: torch.FloatTensor):
     h = self.encoder(x)
     return self.quant_conv(h)
 
-
 def decode(self, z: torch.FloatTensor, return_dict: bool = False):
     return AutoencoderKL.decode(self, z, return_dict)
 
-print(vae)
 vae.encode = encode.__get__(vae, AutoencoderKL)
 vae.decode = decode.__get__(vae, AutoencoderKL)
-traced_vae = torch.jit.trace_module(vae, {"encode": [torch.ones((1, 3, 512, 512), dtype=torch.float32)],
-                                          "decode": [latents]})
-torch.jit.save(traced_vae, "vae_model.pt")
-torch.jit.save(traced_unet, "unet_model.pt")
+scripted_vae = torch.jit.script(vae)
+
+torch.jit.save(scripted_vae, "vae_model.pt")
+torch.jit.save(scripted_unet, "unet_model.pt")
 torch.jit.save(traced_text, "clip_model.pt")
