d27e4034ca59a944d3fc8ea5b2afc041909bd619
arisia <starlights2283@gmail.com>
2023-11-18 21:08:05 +0900

    add file loader && text infomation loader

--- src/prompt.java
+++ src/prompt.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.io.File;
 
 public class prompt {
     public static void main(String[] args) {
@@ -20,25 +21,62 @@ public class prompt {
             System.out.print("메뉴를 선택하세요 : ");
             int value = sc.nextInt();
 //            value를 통해 메뉴 선택
+            String core;
             switch(value){
 //                -> 를 사용한 switch 문은 break가 필요하지 않음
                 case 1 -> {
-                    System.out.println("Get System Info - Using dxdiag");
-                    try {
-                        String filePath = "./dxdiag.txt";
-                        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "dxdiag", "/t", filePath);
-                        Process p = pb.start();
-                        p.waitFor();
-
-                        BufferedReader br = new BufferedReader(new FileReader(filePath));
-                        String line;
-                        while ((line = br.readLine()) != null) {
-                            if (line.trim().startsWith("Card name:") || line.trim().startsWith("Current Mode:")) {
-                                System.out.println(line.trim());
-                            }
+                    Scanner scs = new Scanner(System.in);
+                    System.out.println("시스템 정보를 txt 파일로 출력하시겠습니까? ( 기본설정 : 텍스트 생성 안함 ) ");
+                    System.out.print("텍스트 파일로 생성하고 싶으시다면 yes를 입력해주세요 : ");
+                    core = scs.nextLine();
+                    if(core.equals("yes")){
+                        System.out.println("텍스트 파일을 생성합니다. ( Create Text File )");
+                        System.out.println("Get System Info - Using dxdiag");
+                        try {
+                            String filePath = "./dxdiag.txt";
+                            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "dxdiag", "/t", filePath);
+                            Process p = pb.start();
+                            p.waitFor();
+                        } catch (Exception e) {
+                            System.out.println("에러가 발생하였습니다. 시스템에 GPU 자원이 존재하는지 확인하세요.");
                         }
-                    } catch (Exception e) {
-                        System.out.println("에러가 발생하였습니다. 시스템에 GPU 자원이 존재하는지 확인하세요.");
+                        System.out.println("텍스트 파일이 생성되었습니다.");
+                    }else {
+                        System.out.println("텍스트 파일을 생성하지 않습니다. ( Not Create Text File )");
+                        System.out.println("Get System Info - Using dxdiag");
+                        System.out.println("GPU(렌더링) 항목이 없다면 프로그램이 CPU 모드로 실행됩니다.");
+                        System.out.println("연산에 많은 시간이 소요될 수 있으니 주의해주시길 바랍니다.");
+                        try {
+                            String filePath = "./log.txt";
+                            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "dxdiag", "/t", filePath);
+                            Process p = pb.start();
+                            p.waitFor();
+
+                            BufferedReader br = new BufferedReader(new FileReader(filePath));
+                            String line;
+                            System.out.println("------SYSTEM INFO------");
+                            while ((line = br.readLine()) != null) {
+                                if (line.trim().startsWith("Card name:") || line.trim().startsWith("Current Mode:")) {
+                                    System.out.println(line.trim());
+                                }
+                            }
+                            br.close();
+                            File file = new File(filePath);
+                            try{
+                                file.delete();
+                            } catch (Exception e) {
+                                System.out.println("log파일 삭제에 실패하였습니다.");
+                            } finally {
+                                file.delete();
+                                try {
+                                    br.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } catch (Exception e) {
+                            System.out.println("에러가 발생하였습니다. 시스템에 GPU 자원이 존재하는지 확인하세요.");
+                        }
                     }
                 }
                 case 2 -> {
