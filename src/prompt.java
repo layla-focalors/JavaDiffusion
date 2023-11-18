06f6f79dec0b0ffe339279abe5f652445407ae99
arisia <starlights2283@gmail.com>
2023-11-18 21:25:25 +0900

    add models

--- src/prompt.java
+++ src/prompt.java
@@ -4,10 +4,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class prompt {
     public static void main(String[] args) {
         boolean isexit = false;
+//        모델 이름과 주소 저장
+        String model_Name;
+        String fileURL = "https://github.com/layla-focalors/JavaDiffusion/models/";
 //        isexit 객체를 사용해 프로그램의 종료 여부를 판단
         Scanner sc = new Scanner(System.in);
 //        sc를 통해 스캐너 객체 생성
@@ -16,7 +24,8 @@ public class prompt {
             System.out.println("-------- Menu --------");
             System.out.println("1. 시스템 상태 체크 ( Get System Info )");
             System.out.println("2. 이미지 생성 ( Create Image )");
-            System.out.println("3. 프로그램 종료 ( Exit Program )");
+            System.out.println("3  모델 다운로드 ( Download Model )");
+            System.out.println("4. 프로그램 종료 ( Exit Program )");
             System.out.println("----------------------");
             System.out.print("메뉴를 선택하세요 : ");
             int value = sc.nextInt();
@@ -47,22 +56,31 @@ public class prompt {
                         System.out.println("GPU(렌더링) 항목이 없다면 프로그램이 CPU 모드로 실행됩니다.");
                         System.out.println("연산에 많은 시간이 소요될 수 있으니 주의해주시길 바랍니다.");
                         try {
+//                            파일 경로 생성
                             String filePath = "./log.txt";
+//                            dxdiag(원도우 시스템 매니저)에 시스템 정보 요청
                             ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "dxdiag", "/t", filePath);
+//                            프로세스 메서드 실행
                             Process p = pb.start();
+//                            동작 대기
                             p.waitFor();
 
+//                            파일 정보 읽어오기
                             BufferedReader br = new BufferedReader(new FileReader(filePath));
                             String line;
                             System.out.println("------SYSTEM INFO------");
+//                            반복문 순회, 라인 정보 출력
                             while ((line = br.readLine()) != null) {
                                 if (line.trim().startsWith("Card name:") || line.trim().startsWith("Current Mode:")) {
                                     System.out.println(line.trim());
                                 }
                             }
+//                            파일 탐색 객체 종료
                             br.close();
+//                            파일 경로 재설정, 파일 메서드 참조 파일 생성
                             File file = new File(filePath);
                             try{
+//                                파일 삭제
                                 file.delete();
                             } catch (Exception e) {
                                 System.out.println("log파일 삭제에 실패하였습니다.");
@@ -74,15 +92,26 @@ public class prompt {
                                     e.printStackTrace();
                                 }
                             }
+//                            에러처리
                         } catch (Exception e) {
                             System.out.println("에러가 발생하였습니다. 시스템에 GPU 자원이 존재하는지 확인하세요.");
                         }
                     }
                 }
                 case 2 -> {
+//                    이미지 생성 : stable diffusion loader
                     System.out.println("Create Image");
                 }
                 case 3 -> {
+                  System.out.println("----- Model Downloader -----");
+                  Scanner models = new Scanner(System.in);
+                  System.out.println("!TIP : 모델은 model 폴더에 저장됩니다.");
+                  System.out.print("다운로드 할 모델의 이름을 입력하세요 : ");
+                  model_Name = models.nextLine();
+
+                }
+                case 4 -> {
+//                    프로그램 종료
                     System.out.println("Exit Program");
                     isexit = true;
                 }
